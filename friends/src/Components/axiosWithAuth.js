4060477c421285db475c6161a8ebda3b8d6210fe
taty2010 <shydalys123@gmail.com>
2020-04-03 00:27:39 -0500

    updated api link

--- friends/src/Components/axiosWithAuth.js
+++ friends/src/Components/axiosWithAuth.js
@@ -8,6 +8,6 @@ export const axiosWithAuth = () => {
         headers: {
             Authorization: token
         },
-        baseURL: 'http://localhost:4002/api'
+        baseURL: 'https://lambda-friends.herokuapp.com/api'
     });
 };
